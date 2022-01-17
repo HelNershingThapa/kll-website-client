da48cb3847263435db2a7020762afc2ef09e2d1a
Hel Nershing Thapa <helnershingthapa@gmail.com>
2022-01-17 21:22:01 +0545

    social links added to blog share

--- components/blog/Share.js
+++ components/blog/Share.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
@@ -24,15 +25,30 @@ const useStyles = makeStyles((theme) => ({
 
 const Tags = () => {
   const classes = useStyles();
+  const router = useRouter();
+
+  console.log("router", window.location.href);
   return (
     <Fragment>
       <Typography variant="body1" className={classes.title}>
         Loved it? Share Now!
       </Typography>
       <div className={classes.socialIcons}>
-        <i className={clsx("ri-facebook-circle-fill", classes.socialIcon)} />
-        <i className={clsx("ri-twitter-fill", classes.socialIcon)} />
-        <i className={clsx("ri-linkedin-box-fill", classes.socialIcon)} />
+        <a
+          href={`https://facebook.com/sharer/sharer.php?u=${window.location.href}`}
+        >
+          <i className={clsx("ri-facebook-circle-fill", classes.socialIcon)} />
+        </a>
+        <a
+          href={`https://twitter.com/intent/tweet/?url=${window.location.href}`}
+        >
+          <i className={clsx("ri-twitter-fill", classes.socialIcon)} />
+        </a>
+        <a
+          href={`https://linkedin.com/sharing/share-offsite/?url=${window.location.href}`}
+        >
+          <i className={clsx("ri-linkedin-box-fill", classes.socialIcon)} />
+        </a>
         <i className={clsx("ri-link", classes.socialIcon)} />
       </div>
     </Fragment>
